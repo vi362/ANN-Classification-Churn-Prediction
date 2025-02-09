92626ad9f962f049c6a159f21809a33815c91deb
Vinay Kumar <127966705+vi362@users.noreply.github.com>
2025-02-09 21:35:23 +0530

    ann_classification.py

--- ann_classification.py
+++ ann_classification.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
+
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import pickle
 
